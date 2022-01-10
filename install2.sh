f6aaddd0b8bc42cb55611d1238b6c1f6a5d5cb1c
DomLev <40503681+DomLev@users.noreply.github.com>
2022-01-10 21:31:21 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+desktop="$(cat <<-EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=dolphin
+Comment=
+Exec=/opt/dolphin --no-sandbox
+Icon=
+Path=
+Terminal=false
+StartupNotify=false
+EOF
+
 echo -n "Installing Dolphin {anty}... "
 wget "https://github.com/dolphinrucom/anty-releases/releases/download/v2022.7.10/dolphin-anty-linux-x86_64-latest.AppImage" -O /opt/dolphin &> /dev/null
 chmod +x /opt/dolphin
