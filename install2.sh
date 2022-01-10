9f0105efd499b8e7337197e461d9932253386dbc
DomLev <40503681+DomLev@users.noreply.github.com>
2022-01-10 21:28:18 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,1 +1,12 @@
+echo -n "Installing Dolphin {anty}... "
+wget "https://github.com/dolphinrucom/anty-releases/releases/download/v2022.7.10/dolphin-anty-linux-x86_64-latest.AppImage" -O /opt/dolphin &> /dev/null
+chmod +x /opt/dolphin
+mkdir -p /root/Desktop/
+echo "$desktop" > /root/Desktop/dolphin.desktop
+echo "${GREEN}Done${NC}"
 
+read -r -p "Reboot? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    reboot
+fi
