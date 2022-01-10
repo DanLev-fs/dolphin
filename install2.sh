0df62d44fc9b71c0b89b881c89b098551d24f9cb
DomLev <40503681+DomLev@users.noreply.github.com>
2022-01-10 21:43:11 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-desktopIcon="$(cat <<-EOF
+desktopIconvar="$(cat <<-EOF
 [Desktop Entry]
 Version=1.0
 Type=Application
@@ -22,7 +22,7 @@ echo -n "Installing Dolphin {anty}... "
 wget "https://github.com/dolphinrucom/anty-releases/releases/download/v2022.7.10/dolphin-anty-linux-x86_64-latest.AppImage" -O /opt/dolphin &> /dev/null
 chmod +x /opt/dolphin
 mkdir -p /root/Desktop/
-echo "$desktopIcon" > /root/Desktop/dolphin.desktop
+echo "$desktopIconvar" > /root/Desktop/dolphin.desktop
 echo "${GREEN}Done${NC}"
 
 read -r -p "Reboot? [y/N] " response
