89f11e8761ef7d700af4b6e83a8b509696b65b87
DomLev <40503681+DomLev@users.noreply.github.com>
2022-01-10 21:51:11 +0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -26,7 +26,7 @@ echo "$desktopIconvar" > /root/Desktop/dolphin.desktop
 echo "${GREEN}Done${NC}"
 
 read -r -p "Reboot? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]];
 then
-    reboot
+    reboot;
 fi
