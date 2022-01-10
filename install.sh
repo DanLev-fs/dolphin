45db7e22195e09f41c8f15112864f4c5da0ca5de
DomLev <40503681+DomLev@users.noreply.github.com>
2022-01-10 19:48:24 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+xfce4settings="$(cat <<-EOF
+#!/bin/sh
+if [ -r /etc/default/locale ]; then
+  . /etc/default/locale
+  export LANG LANGUAGE
+fi
+startxfce4
+EOF
+)"
+
+desktop="$(cat <<-EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=dolphin
+Comment=
+Exec=/opt/dolphin --no-sandbox
+Icon=
+Path=
+Terminal=false
+StartupNotify=false
+EOF
+)"
+
+function checkError {
+	if [ $? -eq 1 ] 
+	then 
+		echo "${RED}Error${NC}"
+		exit 1 
+	fi
+}
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo -n "Apt update... "
+apt-get update 1> /dev/null
+checkError
+echo "${GREEN}Done${NC}"
+
+echo -n "Installing XFCE4..."
+apt-get install xfce4 xfce4-terminal tango-icon-theme -y &> /dev/null
+checkError
+echo "${GREEN}Done${NC}"
+
+echo -n "Installing XRDP... "
+apt-get install xrdp -y &> /dev/null
+checkError
+echo "${GREEN}Done${NC}"
+
+echo -n "Configuring... "
+echo xfce4-session >~/.xsession &> /dev/null
+echo "$xfce4settings" > /etc/xrdp/startwm.sh
+echo "${GREEN}Done${NC}"
+
+systemctl enable xrdp &> /dev/null
+
+echo -n "Installing Dolphin {anty}... "
+wget "https://github.com/dolphinrucom/anty-releases/releases/download/v2022.7.10/dolphin-anty-linux-x86_64-latest.AppImage" -O /opt/dolphin &> /dev/null
+chmod +x /opt/dolphin
+echo "$desktop" > /root/Desktop/dolphin.desktop
+if [ $? -eq 1 ]
+then
+	echo "$desktop" > /root/Рабочий\ стол/dolphin.desktop
+fi
+echo "${GREEN}Done${NC}"
+
+read -r -p "Reboot? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    reboot
+fi
