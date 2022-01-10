6c3a65cc41a4d0296095dfceedd91464e76cd856
DomLev <40503681+DomLev@users.noreply.github.com>
2022-01-10 21:28:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,11 +65,8 @@ systemctl enable xrdp &> /dev/null
 echo -n "Installing Dolphin {anty}... "
 wget "https://github.com/dolphinrucom/anty-releases/releases/download/v2022.7.10/dolphin-anty-linux-x86_64-latest.AppImage" -O /opt/dolphin &> /dev/null
 chmod +x /opt/dolphin
+mkdir -p /root/Desktop/
 echo "$desktop" > /root/Desktop/dolphin.desktop
-if [ $? -eq 1 ]
-then
-	echo "$desktop" > /root/Рабочий\ стол/dolphin.desktop
-fi
 echo "${GREEN}Done${NC}"
 
 read -r -p "Reboot? [y/N] " response
